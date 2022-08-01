b7dd24d0c223bc75cdabbdb20d4a64010a2a51e8
GopalDas <gopalkrdas1997@gmail.com>
2022-08-01 20:41:38 +0530

    Added: Permutation of the string

---
+++ src/com/bridgelabz/Permutations.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Permutations {
+    public static void main(String[] args) {
+        System.out.println("Please enter the String for permutation : ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println("Results are : ");
+        permute("",str);
+    }
+    private static void permute(String prefix, String str ) {
+
+        int len = str.length();
+        if (len == 0){
+            System.out.println(prefix);
+        }
+        else {
+            for (int i = 0; i < len; i++){
+                permute(prefix + str.charAt(i), str.substring(0,i) +
+                        str.substring(i+1));
+            }
+        }
+
+    }
+}
